7273caf3e37cadecae061ba3c1756f3167d03c62
Breno Orem <breno.orem@gmail.com>
2026-03-30 21:21:22 -0300

    Adição de Exercícios

---
+++ Extras/ex3-lista-dinamica-encadeada/ListaDinEncad.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ListaDinEncad.h"
+
+struct elemento{
+    struct aluno dados;
+    struct elemento* prox;
+};
+typedef struct elemento Elem;
+
+Lista* cria_lista(){
+    Lista* li = (Lista*)malloc(sizeof(Lista));
+    if(li!=NULL)
+        li == NULL;
+    return li;
+}
+
+void libera_lista(Lista* li){
+    if(li!=NULL){
+        Elem* no;
+        while ((*li) != NULL){
+            no = *li;
+            *li = (*li)->prox;
+            free (no);
+        }
+        free(li);
+    }
+}
+
+int tamanho_lista(Lista* li){
+    if(li==NULL)
+        return 0;
+    
+    int cont=0;
+    Elem* no = *li;
+    
+    while(no != NULL){
+        cont++;
+        no = no->prox;
+    }
+}
+
+int lista_vazia(Lista* li){
+    if(li == NULL)
+        return 1;
+    if(*li == NULL)
+        return 1;
+    return 0;
+}
+
+int insere_lista_inicio(Lista* li, struct aluno al){
+    if(*li == NULL)
+        return 0;
+    Elem* no = (Elem*)malloc(sizeof(Elem));
+    if(no == NULL)
+        return 0;
+    no->dados = al;
+    no->prox = (*li);
+    *li = no;
+    return 1;
+}
+
+int insere_lista_final(Lista* li, struct aluno al){
+    if(*li == NULL)
+        return 0;
+    Elem* no = (Elem*)malloc(sizeof(Elem));
+    if(no == NULL)
+        return 0;
+    
+    no->dados = al;
+    no->prox = NULL;
+
+    if((*li)==NULL)
+        *li = no;
+    else{
+        Elem* aux = *li;
+        while(aux != NULL)
+            aux = aux->prox;
+    
+        aux->prox = no;
+    }  
+}
+
+int insere_lista_ordenada(Lista* li, struct aluno al){
+    if(*li == NULL)
+        return 0;
+    Elem* no = (Elem*)malloc(sizeof(Elem));
+    if(no==NULL)
+        return 0;
+    no->dados = al;
+    if(lista_vazia(li)){
+        no->prox = *li;
+        *li = no;
+        return 1;
+    }
+    else{
+        Elem *ant, *atual = *li;
+        while(atual != NULL && atual->dados.matricula < al.matricula){
+            ant = atual;
+            atual = atual->prox;
+        }
+
+        if(atual == *li){
+            no->prox = (*li);
+            *li = no;
+        }
+        else{
+            no->prox = ant->prox;
+            ant->prox = no;
+        }
+        return 1;
+    }
+        
+}
